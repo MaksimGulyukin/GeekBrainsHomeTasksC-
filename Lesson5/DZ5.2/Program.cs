a9d5e2a2892b0827f0f026d05be3b4e3effc3e2a
Maksim <maksim.gulukin@gmail.com>
2021-11-25 18:52:46 +0300

    Add 5 homework

---
+++ Lesson5/DZ5.2/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ5._2
+{
+   
+    //Гулюкин
+    //. Разработать статический класс Message, содержащий следующие статические методы для обработки текста:
+    //а) Вывести только те слова сообщения, которые содержат не более n букв.
+   //б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+   //в) Найти самое длинное слово сообщения.
+   //г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+    static class Message
+    {
+        static public string text;
+
+        static Message()
+        {
+            text = "Медведь пришел к улью пчёл ночью и украл их мёд ";
+        }
+
+
+        static public void GetWordsByLength(int len)
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            Console.WriteLine("Вывод слов, длинной не более " + len + ": ");
+            foreach (string word in words)
+            {
+                if (word == "")
+                    continue;
+                if (word.Length <= len)
+                    Console.Write(word + " ");
+            }
+        }
+
+
+        static public void DeleteWordByEndChar(char ch)
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            Console.WriteLine("Будут удалены слова, оканчивающиеся на символ '" + ch + "': ");
+            foreach (string word in words)
+            {
+                if (word == "")
+                    continue;
+                if (word[word.Length - 1] == ch)
+                {
+                    Console.Write(word + " ");
+                    text.Replace(word, "");
+                }
+            }
+            Console.WriteLine("В результате работы метода, исходный текст изменился на: " + text);
+        }
+
+
+        static public string FindMaxLengthWord()
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            string maxWord = words[0];
+            int max = words[0].Length;
+
+            foreach (string word in words)
+            {
+                if (word.Length > max)
+                {
+                    max = word.Length;
+                    maxWord = word;
+                }
+            }
+            Console.WriteLine("Слово с самой большой длинной: " + maxWord);
+            return maxWord;
+        }
+
+
+        static public StringBuilder GetLongWordsString()
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            StringBuilder result = new StringBuilder();
+            int max = Message.FindMaxLengthWord().Length;
+            foreach (string word in words)
+            {
+                if (word.Length == max)
+                {
+                    result.Append(word.ToLower() + " ");
+                }
+            }
+            Console.WriteLine("Полученная строка самых длинных слов: " + result);
+            return result;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует программа демонстрации возможностей статического класса Message.");
+
+            Console.WriteLine("\nИмеется следующий текст: \n" + Message.text);
+
+            Console.WriteLine("\nВыведем слова текста, которые содержат не более 5 букв:");
+            Message.GetWordsByLength(5);
+
+            Console.WriteLine();
+            Console.Write("\nУдалим из текста слова, заканчивающиеся на 'л': ");
+            Message.DeleteWordByEndChar('л');
+
+            Console.WriteLine();
+            Console.WriteLine("\nСамое длинное слово в тексте: " + Message.FindMaxLengthWord());
+
+
+            Console.WriteLine("\nСформированная строка StringBuilder из самых длинных слов сообщения: \n" + Message.GetLongWordsString());
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
